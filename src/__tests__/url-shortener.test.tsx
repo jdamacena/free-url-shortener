6cf1b570543c855d59dc8c868b03c09b57904356
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2025-09-04 02:49:19 -0300

    changed url-shortener.test to run on bun

--- src/__tests__/url-shortener.test.tsx
+++ src/__tests__/url-shortener.test.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import UrlShortener from '../components/UrlShortener';
 
+if (typeof window === 'undefined') {
+  // Polyfill window, document, navigator, and text encoders for environments without jsdom (e.g., Bun test runner)
+  // @ts-ignore
+  const { TextEncoder, TextDecoder } = require('util');
+  // @ts-ignore
+  global.TextEncoder = TextEncoder;
+  // @ts-ignore
+  global.TextDecoder = TextDecoder;
+  // @ts-ignore
+  const { JSDOM } = require('jsdom');
+  const dom = new JSDOM('<!doctype html><html><body></body></html>');
+  // @ts-ignore
+  global.window = dom.window;
+  // @ts-ignore
+  global.document = dom.window.document;
+  // @ts-ignore
+  global.navigator = dom.window.navigator;
+}
+
 describe('UrlShortener component', () => {
   it('renders input and button', () => {
     const ref = React.createRef<HTMLInputElement>();
